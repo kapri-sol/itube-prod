bf3581d527f732c8f601fafae2cddeda7d719e74
kapri-sol <ksm6902@gmail.com>
2019-10-18 00:05:04 +0900

    file url

--- src/api/Post/File.resolvers.ts
+++ src/api/Post/File.resolvers.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../../generated/prisma-client";
 import { getExtOfFile } from "../../utils/fileManage";
+import "../../env";
 
 export default {
   File: {
@@ -7,19 +8,18 @@ export default {
       const file = await prisma.file({ id });
       try {
         if (file) {
-          const { id, mimetype, filename, createdAt } = file;
+          const { id, filename } = file;
           const url =
-            "http://localhost:4000/" +
-            mimetype +
-            "/" +
-            createdAt.substring(0, 4) +
-            "/" +
-            createdAt.substring(5, 7) +
-            "/" +
-            createdAt.substring(8, 10) +
-            "/" +
-            id +
-            getExtOfFile(filename);
+            // "http://localhost:4000/" +
+            // mimetype +
+            // "/" +
+            // createdAt.substring(0, 4) +
+            // "/" +
+            // createdAt.substring(5, 7) +
+            // "/" +
+            // createdAt.substring(8, 10) +
+            // "/" +
+            process.env.STORAGE_URL + id + getExtOfFile(filename);
           return url;
         } else {
           return "null";
