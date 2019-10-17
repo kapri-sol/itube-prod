b5ed6104f5e42b8a058235e8f747ec42e707f96d
kapri-sol <ksm6902@gmail.com>
2019-10-17 11:14:13 +0900

    reback

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import decodeJWT from "./utils/decodeJWT";
 import "./env";
 // import { graphqlUploadExpress } from "graphql-upload";
 import { apolloUploadExpress } from "apollo-upload-server";
-import graphqlHTTP from "express-graphql";
+// import graphqlHTTP from "express-graphql";
 
 class App {
   public app: GraphQLServer;
@@ -32,12 +32,10 @@ class App {
     this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
     this.app.express.use(this.jwt);
-    this.app.express
-      .use(
-        // graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 })
-        apolloUploadExpress()
-      )
-      .use(graphqlHTTP({ schema }));
+    this.app.express.use(
+      // graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 })
+      apolloUploadExpress()
+    );
   };
 
   private jwt = async (
